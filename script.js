240c3918708efcbe9646fd12f455ac1be09f90eb
Itseb7 <sebaalbehabeb@gmail.com>
2025-02-26 05:20:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from "firebase/auth";
-import { getFirestore, setDoc, doc, serverTimestamp } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase Configuration (اتركها كما هي)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyClE1Pacj0iGZ2rOY7YMPHdfwoUzuYu8ow",
   authDomain: "swee-98fd1.firebaseapp.com",
@@ -17,8 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // إحضار نموذج التسجيل
 const form = document.getElementById('signupForm');
